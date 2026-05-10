fb6db6674c54315b4d9176ee9ff0d558fb44c3c9
Santiago Romero <santiago_romero82231@poli.edu.co>
2026-05-09 21:23:04 -0500

    Actualizacion cambio de noticia a novedades y añadir imagenes en mds

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,19 +1,20 @@
 import { defineCollection, z } from "astro:content";
 import { glob } from "astro/loaders";
 
-const noticias = defineCollection({
+const novedades = defineCollection({
   loader: glob({
     pattern: "**/*.md",
-    base: "./src/content/noticias",
+    base: "./src/content/novedades",
   }),
 
   schema: z.object({
     title: z.string(),
     description: z.string(),
     date: z.coerce.date(),
+    image: z.string(),
   }),
 });
 
 export const collections = {
-  noticias,
+  novedades,
 };
--- src/content/novedades/PrimerTaller.md
+++ src/content/novedades/PrimerTaller.md
@@ -2,6 +2,7 @@
 title: "Primer Taller Permanezco en ti"
 description: "Solución de ejercicios de talleres DPAS."
 date: 2026-05-08
+image: "/images/novedades/Logo Pemanezco en Ti.png"
 ---
 
 Este viernes estaremos resolviendo dudas y ejercicios de los talleres de DPAS 1 y 2.
